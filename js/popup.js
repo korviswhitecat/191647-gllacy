785ae044eaabd2f089776d146f64cb9f34a1183f
korviswhitecat <eclypsis@mail.ru>
2016-05-17 22:19:56 +0300

    единообразие наше все

--- js/popup.js
+++ js/popup.js
@@ -5,24 +5,24 @@ var close = popup.querySelector(".feedback_button_close");
 
 
 link.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.add("popup_display");
-	overlay.classList.add("overlay_display");
+  event.preventDefault();
+  popup.classList.add("popup_display");
+  overlay.classList.add("overlay_display");
 });
 
 
 close.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.remove("popup_display");
-	overlay.classList.remove("overlay_display");
+  event.preventDefault();
+  popup.classList.remove("popup_display");
+  overlay.classList.remove("overlay_display");
 });
 
 
 window.addEventListener("keydown", function(event) {
-	if (event.keyCode === 27) {
-	if (popup.classList.contains("popup_display")) {
-	popup.classList.remove("popup_display");
-	overlay.classList.remove("overlay_display");
-	}
-	}
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("popup_display")) {
+      popup.classList.remove("popup_display");
+      overlay.classList.remove("overlay_display");
+    }
+  }
 });
